c65a642bca5a25166a7ef86b3ed13f30a7e192ab
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-26 17:56:21 -0300

    created 10 pieces and added some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Rolladie from './components/rolladie';
+// import Rolladie from './components/rolladie';
+// import Carrousel from './components/carrousel';
+import Pickacard from './components/pickacard';
 
 function App() {
   return (
     <div >
-    <Rolladie />
+   <Pickacard />
     </div>
   );
 }
---
+++ src/components/pickacard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Pickacard(){
+    return(
+        <div>
+            <h1>Pickacard</h1>
+            <div className="wrapper">
+                <div className="piece">1</div>
+                <div className="piece">2</div>
+                <div className="piece">3</div>
+                <div className="piece">4</div>
+                <div className="piece">5</div>
+                <div className="piece">6</div>
+                <div className="piece">7</div>
+                <div className="piece">8</div>
+                <div className="piece">9</div>
+                <div className="piece">10</div>
+            </div>
+        </div>
+    )
+}
